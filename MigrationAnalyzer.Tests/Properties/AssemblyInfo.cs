8307cad3a0f7401e55006aa9e1dee39b1d86bcc5
alshaikhd <alshaikh@codativity.com>
2024-09-22 16:21:12 +0400

    Add project files.

---
+++ MigrationAnalyzer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MigrationAnalyzer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("MigrationAnalyzer.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2696fa4b-f15b-4d93-9ffc-64e14dd09ed0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MigrationAnalyzer/Analyzers/ApiCompatabilityAnalyzer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MigrationAnalyzer.Analyzers
+{
+    internal class ApiCompatabilityAnalyzer
+    {
+    }
+}
